4936bdbb6444252a9b2ccf640a08834b8f2f97fd
Milana <milana.bijeljanin@gmail.com>
2020-09-29 11:37:30 +0200

    Check if age is matching

--- Utils/Filters/AgesMatchingFilter.cs
+++ Utils/Filters/AgesMatchingFilter.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using Dapper;
 using Microsoft.AspNetCore.Mvc.Filters;
 using SkiTickets.Models;
